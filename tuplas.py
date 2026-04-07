45285d71d6f4344141d161dc8e49c9ea70396682
Luiz Guilherme <lu.costa1402@gmail.com>
2026-04-06 23:21:45 -0300

    Resolvido ex. 74

--- tuplas.py
+++ tuplas.py
@@ -21,4 +21,13 @@
 
 #74
 
+#from random import randint
+
+#n = (randint(0,10),randint(0,10), randint(0,10), randint(0,10), randint(0,10))
+#print('Os números sorteados foram: ', end='')
+#for i in n:
+#    print(f'{i} ', end='')
+#print(f'\nO maior valor sorteado foi {max(n)}.') 
+#print(f'O menor valor sorteado foi {min(n)}')       
+
 
