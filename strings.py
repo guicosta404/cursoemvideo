115a7268f7e198270d886946e1b16b5001da6340
Luiz Guilherme <lu.costa1402@gmail.com>
2026-04-07 23:21:05 -0300

    Adicionando arquivo de alguns exemplos de manipulação de strings.

---
+++ strings.py
@@ -0,0 +1,19 @@
+### strings
+
+#name = input("Enter your name: ")
+#phone_number = str(input("Enter your phone number: "))
+
+#result = len(name)
+#result = name.find("u") 
+#result = name.rfind("u")
+#name.capitalize()
+#name.upper()
+#name.lower()
+#name.isdigit()
+#name.isalpha()
+
+#result = phone_number.count("-")
+#phone_number = phone_number.replace("-", "_")
+#print(phone_number)
+
+#print(help(str))
