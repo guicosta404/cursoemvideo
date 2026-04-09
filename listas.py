bf5f10128ffe73548d2374142a8167c3442dcd71
Luiz Guilherme <lu.costa1402@gmail.com>
2026-04-08 22:31:20 -0300

    Resolvido ex. 80 sobre listas.

--- listas.py
+++ listas.py
@@ -90,3 +90,21 @@
 # valores.sort()
 # print(f"Voce digitou os numeros : {valores}")
 
+#80
+# 
+# valores = list()
+
+# for v in range(0,5):
+#     valor = int(input("Digite um valor: "))
+#     if v == 0 or valor > valores[len(valores)-1]:
+#         valores.append(valor)
+#         print("Adicionado ao final da lista")
+#     else:
+#         pos = 0
+#         while pos < len(valores):
+#             if valor <= valores[pos]:
+#                 valores.insert(pos, valor)
+#                 print(f"Adicionado na posição {pos} da lista.")
+#                 break
+#             pos += 1 
+# print(f"Os valores digitados em ordem foram {valores}: ")                
