6fd64075947b3f1398b47c69b791058c7da74eb6
Luiz Guilherme <lu.costa1402@gmail.com>
2026-04-13 10:58:33 -0300

    Resolvendo ex. 86

--- listas2.py
+++ listas2.py
@@ -1,3 +1,42 @@
+# teste = list()
+# teste.append("Gustavo")
+# teste.append(40)
+# galera = list()
+# galera.append(teste[:])
+# teste[0] = "Maria"
+# teste[1] = 22
+# galera.append(teste[:])
+# print(galera)
+
+
+# galera = [['Joao', 19], ['Ana', 33], ['Joaquim', 13],['Maria', 45]]
+# # print(galera[2][1])
+# for pessoa in galera:
+#     print(f"{pessoa[0]} tem {pessoa[1]} anos de idade.")
+
+
+# galera = []
+# dado = []
+# maior = 0
+# menor = 0
+# for c in range(0,3):
+#     dado.append(str(input("Nome: ")))
+#     dado.append(int(input("Idade: ")))
+#     galera.append(dado[:])
+#     dado.clear()
+# print(galera)
+
+# for pessoa in galera:
+#     if pessoa[1] >= 21:
+#         print(f"{pessoa[0]} é maior de idade")
+#         maior += 1
+#     else:
+#         print(f"{pessoa[0]} é menor de idade")    
+#         menor +=1
+# print(f"Temos {maior} de idade e {menor} menores.")
+
+
+
 # 84
 
 # lista = list()
@@ -47,4 +86,20 @@
 # valores[1].sort()
 # print(f"Os valores pares foram {valores[0]}")
 # print(f"Os valores ímpares foram {valores[1]}")
-# print(valores[0])       
+# print(valores[0])   
+
+
+#86
+
+# matriz = [[0,0,0], [0,0,0], [0,0,0]]
+
+# for linha in range(0,3):
+#     for coluna in range(0,3):
+#         matriz[linha][coluna] = int(input(f'Digite um valor para {linha}, {coluna}: '))
+# for linha in range(0,3):
+#     for coluna in range(0,3):
+#         print(f'[{matriz[linha][coluna]}]', end='')
+#     print()    
+    
+
+
